a9bde15cd71b44c164f5d72b71756218985ecb97
codegirlly <coderly2017@163.com>
2018-02-07 23:29:08 +0800

    ei er

--- lyspringboot/src/main/java/com/ly/controller/LyController.java
+++ lyspringboot/src/main/java/com/ly/controller/LyController.java
@@ -6,12 +6,10 @@ import com.ly.service.LyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
 @Controller
 @RequestMapping("/ly")
